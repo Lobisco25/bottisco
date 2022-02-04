dc1e3b6da5385d5b4b82a2abe8c0e7890b084cab
Lobisco25 <fefeeliaslobo@outlook.com>
2022-02-03 21:49:10 -0300

    📶

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args, user, channel, self) => {
     client.ping().then(function(data) {
         let ping = Math.floor(Math.round(data*1000))
-        client.say(channel, `@${user.username} Check PONG! O bot está online (${ping}ms 📡)`)
+        client.say(channel, `@${user.username} Check PONG! O bot está online (${ping}ms 📶 )`)
     })
 }
