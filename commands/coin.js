1866d408a6216b602807ae17859070e289afba9f
Lobisco25 <fefeeliaslobo@outlook.com>
2022-02-25 15:10:42 -0300

    Coin now has rare chance of falling standing

--- commands/coin.js
+++ commands/coin.js
@@ -2,6 +2,11 @@ exports.run = (client, message, args, user, channel, self) => {
     let num = Math.random();
     if (num < 0.5) {
       client.say(channel, `${user.username} sua moeda caiu em cara! 🪙 `)
+
+    } else if(num < 0.02) {
+      client.say(channel, `${user.username} taporra caiu em pé OMEGADANCE`)
+
+    
     } else {
       client.say(channel, `${user.username} sua moeda caiu em coroa! 🪙 `)
       
